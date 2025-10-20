12dacfee17bd4ef4503280f856edc71a7ba17fa3
Carlos Juanes <carlosjuanes28@hotmail.com>
2025-10-20 19:16:05 -0400

    se finalizo el ejercicio 9 correspondiente a la semana 16

---
+++ EJERCICIOS PY/tarea_semana_16/ejercicios_de_tarea/my_module_homework/exercise_9.py
@@ -0,0 +1,20 @@
+# 10. Create a function that accepts a string and returns how many vowels it contains."
+# "- Example:
+# - phrase = Hola mundo
+# - Result → 4"
+
+def count_vowels(text):
+    if not isinstance(text, str):
+        raise TypeError("error!!! the input must be a string")
+    count = 0
+    for char in text:
+        if char.lower() in "aeiou":
+            count += 1
+    return count
+
+
+my_text_string = "hola mundo" 
+vowel_count = count_vowels(my_text_string) 
+print(f"The word '{my_text_string}' contains {vowel_count} vowels.")
+
+
